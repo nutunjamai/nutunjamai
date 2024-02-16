0d1d454ec5a828b18c1c5402baac879627406b59
scart3r <carter.skylar@proton.me>
2024-02-16 01:05:15 +0000

    updated changes

--- feet_conv.py
+++ feet_conv.py
@@ -1,4 +1,14 @@
 inches_ft = input('Feet converter: enter the number of inches: ')
-int(print('The length in feet is:', inches_ft * 0.0833 ))
+float_inches_ft = float(inches_ft)
+print('The length in feet is:', float_inches_ft * 0.0833 )
+
+
+inches_ft = float(input('Feet converter: enter the number of inches: '))
+print('The length in feet is:', inches_ft * 0.0833 )
+
+
+
+inches_ft = int(input('Feet converter: enter the number of inches: '))
+print('The length in feet is:', inches_ft * 0.0833 )
 
 

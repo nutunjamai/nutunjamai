800a162e42518a4d566f084b30bb5cf1a98e6e0a
scart3r <carter.skylar@proton.me>
2024-02-23 18:32:56 +0000

    list_tuples

---
+++ add_multply_lists.py
@@ -0,0 +1,7 @@
+list_us = ['New York City', 'Chicago']
+list_uk = ['London', 'Bristol']
+list_all = list_us + list_uk
+print(list_all)
+
+numbers = [0, 1] * 10
+print(numbers)
---
+++ tuples_list.py
@@ -0,0 +1,6 @@
+capitals = [('London', 'UK', 8.98), ('Canberra', 'Australia', 0.4), ('Algiers', 'Algeria', 3.9)]
+for capital in capitals:
+    print('Name:', capital[0], 'Country:', capital[1], 'Population:', capital[2])
+    
+user_data = ('John', 'American', 1964, [77.0, 78.2, 77.5])
+user_data[3].append(79.6)
